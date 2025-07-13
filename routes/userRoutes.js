62e66e1b84fa8528e090280c19436352c7000dc4
Priyansh Verma <verma.priyansh444@gmail.com>
2025-07-13 19:45:41 +0530

    feat(routes): add routes for forgot-password and reset-password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { registerUser, authUser, getMe } from '../controllers/userController.js';
+import {
+  registerUser,
+  authUser,
+  getMe,
+  requestPasswordReset,
+  resetPassword,
+} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +13,8 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', authUser);
 router.get('/me', protect, getMe);
+
+router.post('/forgot-password', requestPasswordReset);
+router.post('/reset-password', resetPassword);
 
 export default router;
